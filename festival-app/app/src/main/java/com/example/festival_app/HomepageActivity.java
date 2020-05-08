a97fd5e978acbe13748c29774139cf631ae5269a
Fipzz <Philiprumle@gmail.com>
2020-05-08 23:25:42 +0200

    made project

---
+++ festival-app/app/src/main/java/com/example/festival_app/HomepageActivity.java
@@ -0,0 +1,46 @@
+package com.example.festival_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomepageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView SeeHeatMap, RegisterOnHeatMap;
+    TextView tvSee, tvregister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        SeeHeatMap = findViewById(R.id.imageView1);
+        SeeHeatMap.setImageResource(R.drawable.unnamed);
+        SeeHeatMap.setOnClickListener(this);
+
+        RegisterOnHeatMap = findViewById(R.id.imageView2);
+        RegisterOnHeatMap.setImageResource(R.drawable.unnamed);
+        RegisterOnHeatMap.setOnClickListener(this);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==SeeHeatMap.getId()){
+
+
+
+        } else if (v.getId()==RegisterOnHeatMap.getId()){
+
+
+
+        }
+    }
+}
