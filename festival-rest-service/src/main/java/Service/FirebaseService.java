2ad8d2b418bbbcbb468e4cf04cda151e3526725f
Andi365 <anders1998@live.dk>
2020-05-08 17:00:03 +0200

    added new endpoints into the REST service.

--- festival-rest-service/src/main/java/Service/FirebaseService.java
+++ festival-rest-service/src/main/java/Service/FirebaseService.java
@@ -2,13 +2,12 @@ package Service;
 
 import Objects.User;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -23,19 +22,39 @@ public class FirebaseService {
         return INSTANCE;
     }
 
-    public String getUserDetails(String name) throws ExecutionException, InterruptedException {
+    public Boolean getUserDetails(String email) throws ExecutionException, InterruptedException {
         Firestore db = FirestoreClient.getFirestore();
         CollectionReference col = db.collection("coordinates");
-        ApiFuture<DocumentSnapshot> future = col.document(name).get();
-        return future.get().getUpdateTime().toString();
+        ApiFuture<DocumentSnapshot> future = col.document(email).get();
+        return future.get().exists();
     }
 
-    public String saveUserDetails(User user) throws ExecutionException, InterruptedException {
+    public void saveUserDetails(User user) throws ExecutionException, InterruptedException {
         Firestore db = FirestoreClient.getFirestore();
         CollectionReference col = db.collection("coordinates");
-        ApiFuture<WriteResult> future = col.document(user.getName()).set(user);
+        ApiFuture<WriteResult> future = col.document(user.getEmail()).set(user);
         future.get();
-        return future.get().getUpdateTime().toString();
+    }
+
+    public void deleteUser(User user) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference col = db.collection("coordinates");
+        ApiFuture<WriteResult> future = col.document(user.getEmail()).delete();
+        future.get();
+    }
+
+    public ArrayList<String> getGeoPoints() throws ExecutionException, InterruptedException {
+        ArrayList<String> geoPoints = new ArrayList<>();
+
+        Firestore db = FirestoreClient.getFirestore();
+        // asynchronously retrieve all documents in the coordinates
+        ApiFuture<QuerySnapshot> future = db.collection("coordinates").get();
+        // Gets all geopoints as strings.
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            geoPoints.add(document.getString("g"));
+        }
+        return geoPoints;
     }
 
 
