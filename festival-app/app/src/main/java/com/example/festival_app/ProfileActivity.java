3c48c526e70bcf7b0e66c772eaf633e8e065b5f6
DovendyretS <afck97@hotmail.com>
2020-05-10 02:16:12 +0200

    get users in onStart if they are already logged in

--- festival-app/app/src/main/java/com/example/festival_app/ProfileActivity.java
+++ festival-app/app/src/main/java/com/example/festival_app/ProfileActivity.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button logout, back, delete;
+    private FirebaseAuth mAuth;
+    private UserInformation mUserInformation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,29 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         back = findViewById(R.id.button_back);
         back.setOnClickListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUserInformation = UserInformation.getInstance();
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+            mUserInformation.setUserEmail(currentUser.getEmail());
+
+            currentUser.getIdToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                @Override
+                public void onComplete(@NonNull Task<GetTokenResult> task) {
+                    if (task.isSuccessful()){
+                        mUserInformation.setToken(task.getResult().getToken());
+                    }else
+                        task.getException().printStackTrace();
+                }
+            });
+        }
+    }
 
     @Override
     public void onClick(View view) {
